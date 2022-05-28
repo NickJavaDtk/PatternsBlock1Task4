c6626f7f982ae7fe99112bc7c2d3fd710e54c0af
Nickolay Petrushin <nick.mflj@gmail.com>
2022-05-28 14:14:10 +0300

    for verification

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps( );
+        System.out.println(binOps.sum("11", "1011"));
+        System.out.println(binOps.mult("111", "1001"));
+
+    }
+}
